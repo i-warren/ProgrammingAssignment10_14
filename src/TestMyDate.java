1c0b28a35893a3235f3b55c773aedd61673ea154
Isaac Warren <isaacwarren25@gmail.com>
2024-01-25 13:46:46 -0500

    Created Constructors, all getter methods, setYear, setMonth, setDay.
    Crested TestMyDate to test as per book instructions.

--- src/TestMyDate.java
+++ src/TestMyDate.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class TestMyDate {
+public class TestMyDate {
+    public static void main(String[] args) {
+        MyDate date1 = new MyDate();
+        System.out.println("Date1");
+        System.out.println("Year: " + date1.getYear());
+        System.out.println("Month: " + date1.getMonth());
+        System.out.println("Day: " + date1.getDay());
+        System.out.println("");
+
+        MyDate date2 =  new MyDate(1234567898765L );
+        System.out.println("Date2");
+        System.out.println("Year: " + date2.getYear());
+        System.out.println("Month: " + date2.getMonth());
+        System.out.println("Day: " + date2.getDay());
+        System.out.println("");
+
+    }
 }
