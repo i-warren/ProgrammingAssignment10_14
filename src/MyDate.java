7b4afa839e28777baa18453a41fdbf9b6e669a95
Isaac Warren <isaacwarren25@gmail.com>
2024-01-25 14:19:41 -0500

    added setDate(long elapsedTime), testing for same, added comments throughout

--- src/MyDate.java
+++ src/MyDate.java
@@ -1,11 +1,24 @@
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+/**
+ * MyDate is a Java Class to store date information.
+ * It utilizes java.util.GregorianCalendar for ease
+ * Class has three constructors:
+ * myDate(), myDate(int year, int month, int day), myDate(long elapsedTime)
+ * </p>
+ * @author Isaac Warren
+ */
+
 public class MyDate {
+    // private fields used by MyDate
     private int year;
     private int month;
     private int day;
 
+    /**
+     * Contructs a MyDate object using the current date from GregorianCalendar
+     */
     MyDate(){
         GregorianCalendar date = new GregorianCalendar();
         year = date.get(Calendar.YEAR);
@@ -14,6 +27,10 @@ public class MyDate {
 
     }
 
+    /**
+     * Contructs a MyDate object using the parameter elapsedTime
+     * @param elapsedTime A long number representing elapsed time in ms since Jan 1, 1970
+     */
     MyDate(long elapsedTime) {
         GregorianCalendar date = new GregorianCalendar();
         date.setTimeInMillis(elapsedTime);
@@ -22,37 +39,77 @@ public class MyDate {
         day = date.get(Calendar.DAY_OF_MONTH);
     }
 
+    /**
+     * Constructs a myDate Object using the parameters year, month, day
+     * @param year An integer representing the year
+     * @param month An integer representing the month (Uses Jan = 0)
+     * @param day   An integer representing the day of the month
+     */
     MyDate(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
-    // Getter methods for year, month, and day
+    /**
+     * Returns year
+     * @return Integer representing the year
+     */
     public int getYear(){
         return year;
     }
 
+    /**
+     * Returns month
+     * @return Integer representing the month (Jan = 0)
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     * Returns day
+     * @return Integer representing day of month
+     */
     public int getDay() {
         return day;
     }
 
-    // Setter Methods year, month, and day
+
+    /**
+     * Sets year
+     * @param year Integer representing year
+     */
     public void setYear(int year) {
         this.year = year;
     }
 
+    /**
+     * Sets month
+     * @param month Integer representing the month (Jan = 0)
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     * Sets day
+     * @param day Integer representing day of month
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
 
+    /**
+     * sets year, month, day by using elapsedTime
+     * @param elapsedTime A long number representing elapsed time in ms since Jan 1, 1970
+     */
+    public void setDate(long elapsedTime) {
+        GregorianCalendar date = new GregorianCalendar();
+        date.setTimeInMillis(elapsedTime);
+        year = date.get(Calendar.YEAR);
+        month = date.get(Calendar.MONTH);
+        day = date.get(Calendar.DAY_OF_MONTH);
+    }
 }
--- src/TestMyDate.java
+++ src/TestMyDate.java
@@ -1,18 +1,55 @@
+/**
+ * TestMyDate is a Java Class to test the MyDate class.
+ * It creates two MyDate objects using Mydate() and MyDate(long elapsedTime)
+ * The object information is printed for both objects using getter methods
+ * date1 is modified using setYear(), setMonth(), and setDay()
+ * the object info is printed again to test method
+ * date2 is modified using setDate(long elapsedTime)
+ * the object info is printed again to test method
+ * </p>
+ * @author Isaac Warren
+ */
 public class TestMyDate {
     public static void main(String[] args) {
+        // Creates date1 using default constructor
         MyDate date1 = new MyDate();
+
+        // Prints date1 info
         System.out.println("Date1");
         System.out.println("Year: " + date1.getYear());
         System.out.println("Month: " + date1.getMonth());
         System.out.println("Day: " + date1.getDay());
         System.out.println("");
 
+        // Changes date1 using setter methods on year, month day
+        date1.setYear(1980);
+        date1.setMonth(5);
+        date1.setDay(18);
+
+        //Prints date1 info
+        System.out.println("Date1: Change date using setYear(), setMonth() and setDay()");
+        System.out.println("Year: " + date1.getYear());
+        System.out.println("Month: " + date1.getMonth());
+        System.out.println("Day: " + date1.getDay());
+        System.out.println("");
+
+        // Creates date2 with long integer
         MyDate date2 =  new MyDate(1234567898765L );
+
+        // Prints date2 info
         System.out.println("Date2");
         System.out.println("Year: " + date2.getYear());
         System.out.println("Month: " + date2.getMonth());
         System.out.println("Day: " + date2.getDay());
         System.out.println("");
 
+        // Changes date of date2 using setDate(long elapsedTime)
+        date2.setDate(1434567898765L);
+
+        // Prints date2 info
+        System.out.println("Date2: Change date using setDate(long elapsedTime)");
+        System.out.println("Year: " + date2.getYear());
+        System.out.println("Month: " + date2.getMonth());
+        System.out.println("Day: " + date2.getDay());
     }
 }
